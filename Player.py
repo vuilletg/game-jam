22ecd65a1c112b5987803af4a6c5e54330fd07cc
vuilletg <gaspard.vuillet@gmail.com>
2023-11-06 19:16:22 +0100

    commit hitbox et saut de base

--- Player.py
+++ Player.py
@@ -8,9 +8,31 @@ class player(pygame.sprite.Sprite):
       self.y = y
       self.taille = taille
       self.rect = pygame.Rect(self.x, self.y, self.taille[0], self.taille[1])
+      self.aSauter=False
+      self.saut =0
+      self.saut_monte=0
+      self.saut_decente=10
+      self.nbsaut=0
+
 
     def move(self, vitesse):
        self.rect.x += vitesse
     
     def afficher(self, surface):
        pygame.draw.rect(surface, (255,255,255), self.rect)
+
+    def sauter(self):
+        if self.aSauter:
+            if self.saut_monte>=10:
+                self.saut_decente-=1
+                self.saut = self.saut_decente
+            else:
+                self.saut_monte+=1
+                self.saut = self.saut_monte
+
+            if self.saut_decente<0:
+                self.saut_monte = 0
+                self.saut_decente = 5
+                self.aSauter = False
+
+        self.rect.y= self.rect.y - (10 *(self.saut/2))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from Player import player
+from sol import sol
 import sys
 
 class jeu:
@@ -11,6 +12,13 @@ class jeu:
         self.tailleJoueur = [32,64]
         self.vitesseJoueurX=0
         self.player = player(self.xJoueur,self.yJoueur,self.tailleJoueur)
+        self.sol = sol()
+        self.gravite = (0,10)
+        self.resistance = (0,0)
+        self.rect = pygame.Rect(0,0, 1100, 600)
+        self.colision =False
+        self.horloge = pygame.time.Clock()
+        self.fps= 60
     
     def Boucle_principale (self):
         while self.jeu_encours:
@@ -19,19 +27,39 @@ class jeu:
                     sys.exit()
                 if event.type== pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        self.vitesseJoueurX = 1
+                        self.vitesseJoueurX = 10
                     if event.key == pygame.K_LEFT:
-                        self.vitesseJoueurX = -1
+                        self.vitesseJoueurX = -10
+                    if event.key == pygame.K_SPACE:
+                        self.player.aSauter= True
+                        self.player.nbsaut+=1
                 if event.type== pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
                         self.vitesseJoueurX = 0
                     if event.key == pygame.K_LEFT:
                         self.vitesseJoueurX = 0
 
+            if self.sol.rect.colliderect(self.player.rect):
+                self.resistance = (0, -10)
+                self.colision = True
+                self.player.nbsaut=0
+            else:
+                self.resistance = (0, 0)
+
+            if self.colision and self.player.aSauter and self.player.nbsaut<1:
+                self.player.sauter()
             self.player.move(self.vitesseJoueurX)
+            self.gravite_jeu()
+            self.player.rect.clamp_ip(self.rect)
             self.ecran.fill((0,0,0))
             self.player.afficher(self.ecran)
+            self.sol.afficher(self.ecran)
+            pygame.draw.rect(self.ecran,(255,255,255),self.rect,1)
+            self.horloge.tick(self.fps)
             pygame.display.flip()
+    
+    def gravite_jeu(self):
+        self.player.rect.y +=self.gravite[1] + self.resistance[1]
 
 
 if __name__=='__main__':
