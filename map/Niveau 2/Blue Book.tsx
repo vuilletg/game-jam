cc30886b297ff094c7fb1b8cca20edd0605bf899
Noah669 <dassacn@etu.univ-grenoble-alpes.fr>
2023-11-09 11:46:53 +0100

    Level design Tuto & Level 1

---
+++ map/Niveau 2/Blue Book.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Blue Book" tilewidth="8" tileheight="16" tilecount="1" columns="1">
+ <image source="../../ASSET/niveau 2/Books/Blue Book.png" width="8" height="16"/>
+</tileset>
